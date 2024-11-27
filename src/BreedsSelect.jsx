478c83fa9139ea4acbab2d19a6f03326c582c99a
nihei-shunsuke <c1041462@planet.kanazawa-it.ac.jp>
2024-11-28 01:45:59 +0900

    station10 clear

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,26 @@
 // @ts-check
+import { useEffect, useState } from 'react'
 
-export const BreedsSelect = () => {
-  return <></>
+export const BreedsSelect = (/** @type {{ breeds: { [key: string]: any[] }; setSelectedBreed: (arg0: string) => void; }} */ props) => {
+  useEffect(() => {
+    console.log('BreedsSelect:', props.breeds)
+  }, [props.breeds])
+
+  if (!props.breeds) {
+    return <></>
+  }
+
+  const breedKeys = Object.keys(props.breeds);
+
+  return (
+    <>
+      <select onChange={(e) => props.setSelectedBreed(e.target.value)}>
+        {breedKeys.map((breed) => (
+          <option key={breed} value={breed}>{breed}</option>
+        ))}
+      </select>
+    </>
+  )
 }
 
 export default BreedsSelect
