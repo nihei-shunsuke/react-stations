4feea8073339251e2238973be9d50068a4501749
nihei-shunsuke <c1041462@planet.kanazawa-it.ac.jp>
2024-11-28 00:49:33 +0900

    station9 clear

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,6 +1,19 @@
 // @ts-check
-
+import { useEffect, useState } from 'react'
 export const DogListContainer = () => {
+  const fetchDogBreads = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/list/all')
+      const data = await response.json()
+      setBreads(data.message)
+    } catch (error) {
+      console.error('Error fetching dog breeds:', error)
+    }
+  }
+  const [breads, setBreads] = useState([])
+  useEffect(() => {
+    fetchDogBreads()
+  }, [])
   return <></>
 }
 
